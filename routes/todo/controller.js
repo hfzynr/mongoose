2547cfa87e4346e4f0382b5fab64c03e363622d1
SutantoAdiNugroho <adi.nugroho.an836@gmail.com>
2019-12-02 16:56:52 +0700

    add fitur

--- routes/todo/controller.js
+++ routes/todo/controller.js
@@ -1,4 +1,5 @@
 const {Todos} = require("../../models")
+const objectId = require("mongodb").ObjectId
 
 module.exports = {
     getAll: async(req, res) => {
@@ -10,10 +11,40 @@ module.exports = {
             console.log(error);
         }
     },
+    getByEmail: async(req, res) => {
+        try {
+            const result = await Todos.find({ email: req.params.email }).populate("users")
+
+            res.status(200).json({message: "Show all todos by id", data: result})
+        } catch (error) {
+            console.log(error);
+        }
+    },
     addOne: async(req, res) => {
         try {
-            const result = await Todos.insertOne(req.body)
+            const result = await Todos.create(req.body)
+            
             res.status(200).json({message: "Data successfully added", data: result})
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    deleteOne: async(req, res) => {
+        const { id } = req.params;
+        try {
+            const result = await Todos.remove({ _id: objectId(id) })
+
+            res.status(200).json({message: `Data succesfully delete with id ${id}`, data: result})
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    updateOne: async(req, res) => {
+        const {id} = req.params;
+        try {
+            const result = await Todos.update({ _id : objectId(id) }, {$set : (req.body)})
+
+            res.status(200).json({message: `Data succesfully update with id ${id}`, data: result})
         } catch (error) {
             console.log(error);
         }
--- routes/todo/index.js
+++ routes/todo/index.js
@@ -1,6 +1,9 @@
 const router = require("express").Router()
 
 router.get("/", require("./controller").getAll)
+router.get("/email/:email", require("./controller").getByEmail)
 router.post("/", require("./controller").addOne)
+router.delete("/:id", require("./controller").deleteOne)
+router.put("/:id", require("./controller").updateOne)
 
 module.exports = router
